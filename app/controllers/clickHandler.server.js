aac0b749abcc4c3dae8b517c62d8d7f794cac53a
Abdul <abdulbagasra@gmail.com>
2016-12-02 02:05:35 -0600

    Refactor code for Mongoose

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,46 +1,35 @@
-function clickHandler(db) {
-  var clicks = db.collection('clicks');
+var Clicks = require('../models/clicks.js')
 
+function ClickHandler() {
   this.getClicks = function(req, res) {
-    var clickProjection = {_id: 0};
-    clicks.findOne({}, clickProjection, function(err, result) {
+    Clicks.findOne({}, {_id: 0}).exec(function(err, result) {
       if (err) throw err;
 
       if (result) {
         res.json(result);
       } else {
-        clicks.insert({clicks: 0}, function(err) {
-          if (err) throw err;
-        })
-
-        clicks.findOne({}, clickProjection, function(err, doc) {
+        var newDoc = new Clicks({clicks: 0});
+        newDoc.save(function(err, doc) {
           if (err) throw err;
           res.json(doc);
         });
       }
-
-    });
+    })
   };
 
   this.addClick = function(req, res) {
-    clicks
-          .findAndModify(
-            {},
-            {_id: 1},
-            {$inc: {clicks: 1}},
-            function(err, result) {
-              if (err) throw err;
-              res.json(result);
-            }
-          );
+    Clicks.findOneAndUpdate({}, {$inc: {clicks: 1}}).exec(function(err, result) {
+      if (err) throw err;
+      res.json(result)
+    })
   };
 
   this.resetClicks = function(req, res) {
-    clicks.update({}, {clicks: 0}, function(err, result) {
+    Clicks.findOneAndUpdate({}, {clicks: 0}).exec(function(err, result) {
       if (err) throw err;
       res.json(result);
     })
   }
 }
 
-module.exports = clickHandler;
+module.exports = ClickHandler;
--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 var express = require('express');
     routes = require('./app/routes/index.js');
-    mongo = require('mongodb').MongoClient;
+    mongoose = require('mongoose');
 
 var app = express();
 app.set('port', (process.env.PORT || 8080));
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function(err, db) {
-  if (err) {
-    throw new Error('Database failed to connect!');
-  } else {
-    console.log('MongoDB successfully connected on port 27017');
-  }
+mongoose.connect('mongodb://localhost:27017/clementinejs');
 
-  app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-  routes(app, db);
+routes(app);
 
-  app.listen(app.get('port'), function() {
-    console.log('Clementine app listening on port ' + app.get('port'));
-  })
+app.listen(app.get('port'), function() {
+  console.log('Clementine app listening on port ' + app.get('port'));
 })
