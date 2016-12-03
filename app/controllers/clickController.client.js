a9f016dadaa724538bd8f382eb6d3b08145fca76
Abdul <abdulbagasra@gmail.com>
2016-12-02 22:00:18 -0600

    Finish implementing Passport

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -2,48 +2,26 @@
   var addButton = document.querySelector('.btn-add');
   var deleteButton = document.querySelector('.btn-delete');
   var clickNbr = document.querySelector('#click-nbr');
-  var apiUrl = 'http://localhost:8080/api/clicks';
-
-  function ready(fn) {
-    if (typeof fn != 'function')
-      return;
-
-    if (document.readyState === 'complete')
-      return fn();
-
-    document.addEventListener('DOMContentLoaded', fn, false);
-  }
-
-  function ajaxRequest(method, url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-        callback(xmlHttp.response);
-    };
-
-    xmlHttp.open(method, url, true);
-    xmlHttp.send()
-  }
+  var apiUrl = appUrl + '/api/:id/clicks';
 
   function updateClickCount(data) {
     var clickObject = JSON.parse(data);
     clickNbr.innerHTML = clickObject.clicks;
   }
 
-  ready(ajaxRequest('GET', apiUrl, updateClickCount));
+  ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount));
 
   addButton.addEventListener('click', function() {
-    ajaxRequest('POST', apiUrl, function() {
-      ajaxRequest('GET', apiUrl, updateClickCount);
+    ajaxFunctions.ajaxRequest('POST', apiUrl, function() {
+      ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
     })
   }, false);
 
   deleteButton.addEventListener('click', function() {
-    ajaxRequest('DELETE', apiUrl, function() {
-      ajaxRequest('GET', apiUrl, updateClickCount)
+    ajaxFunctions.ajaxRequest('DELETE', apiUrl, function() {
+      ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount)
     })
   }, false);
 
-  
+
 })();
--- app/models/clicks.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Click = new Schema(
-    {clicks: Number},
-    {versionKey: false}
-  );
-
-module.exports = mongoose.model('Click', Click);
